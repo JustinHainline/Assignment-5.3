ed701bcda27d4822e50d7058a75c042195c21615
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-06-04 08:06:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment_5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 10 single digit numbers: ");
+            int[] nums = new int[10];
+
+            for (int i =0; i < 10; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+         
+            bool ContainsDuplicate(int[] nums)
+            {
+                return nums.Length != nums.Distinct().Count(); 
+            }
+
+            bool hasDuplicates = ContainsDuplicate(nums);
+            Console.WriteLine($"Contains duplicates: {hasDuplicates}");
+        }
+    }
+}
